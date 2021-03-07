242aa10883eb4b5802c0dc5fc52850eb336b915c
KenshuHori <kenshuhori@gmail.com>
2021-03-07 12:04:32 +0900

    認識文字列をファイルに書き込んでみる

--- src/public/image.js
+++ src/public/image.js
@@ -1,6 +1,9 @@
 const { createWorker } = require("tesseract.js");
+const fs = require("fs");
 
-const worker = createWorker();
+const worker = createWorker({
+  logger: m => console.log(m)
+});
 
 async function ocr(filename) {
   await worker.load();
@@ -9,8 +12,16 @@ async function ocr(filename) {
   const {
     data: { text }
   } = await worker.recognize(filename);
+  await output_file(text);
   console.log(text);
   await worker.terminate();
+};
+
+async function output_file(text) {
+  fs.writeFile("output.txt", text, (err) => {
+    if (err) throw err;
+    console.log('正常に書き込みが完了しました');
+  });
 };
 
 let filename = process.argv[2];
