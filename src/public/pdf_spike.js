222295eaf2dd1d11108ef23e33a9967c0f9554fb
Kenshu Hori <kenshuhori@gmail.com>
2021-03-24 14:11:08 +0900

    資産合計、負債合計、純資産合計を取得できるようにする

--- src/public/pdf_spike.js
+++ src/public/pdf_spike.js
@@ -1,15 +1,14 @@
 //. pdf-sample.js
-var fs = require( 'fs' ),
+let fs = require( 'fs' ),
     pdf = require( 'pdf-parse' );
-const { isNumber } = require('util');
+let Asset = require('./model/asset.js');
+let Liability = require('./model/liability.js');
+let NetAsset = require('./model/net_asset.js');
 
 // default render callback
 function render_page(pageData) {
-  //check documents https://mozilla.github.io/pdf.js/
   let render_options = {
-    //replaces all occurrences of whitespace with standard spaces (0x20). The default value is `false`.
     normalizeWhitespace: true,
-    //do not attempt to combine same line TextItem's. The default value is `false`.
     disableCombineTextItems: false
   }
 
@@ -18,9 +17,6 @@ function render_page(pageData) {
     let lastY, text = '';
     for (let item of textContent.items) {
       pure_str = item.str.split(',').join('');
-      console.log(pure_str)
-      console.log(isNaN(Number(pure_str)) == false)
-      console.log(pure_str == '―')
       if (isNaN(Number(pure_str)) == false || pure_str == '―' || pure_str.charAt(0) == '△') {
         item.str = ' ' + item.str;
       }
@@ -39,12 +35,76 @@ let options = {
   pagerender: render_page
 }
 
+function assetBuilder(text) {
+  const category_begin = "資産の部";
+  const category_end = "資産合計"
+  let amount_current_asset, amount_fixed_asset = 0;
+  const category_data = text.slice(text.indexOf(category_begin) + category_begin.length).trim();
+  const rows = category_data.split('\n');
+  rows.some(function(row_str) {
+    let row = row_str.split(' ').filter(r => r !== "");
+    if(row.length == 3 && row[0] == "流動資産合計") {
+      amount_current_asset = row[2];
+    } else if(row.length == 3 && row[0] == "固定資産合計") {
+      amount_fixed_asset = row[2];
+    } else if(row.length == 3 && row[0] == category_end) {
+      return true; // ループ終了
+    }
+  });
+  return new Asset(amount_current_asset, amount_fixed_asset);
+}
+
+function liabilityBuilder(text) {
+  const category_begin = "負債の部";
+  const category_end = "負債合計"
+  let amount_current_liability, amount_fixed_liability = 0;
+  const category_data = text.slice(text.indexOf(category_begin) + category_begin.length).trim();
+  const rows = category_data.split('\n');
+  rows.some(function(row_str) {
+    let row = row_str.split(' ').filter(r => r !== "");
+    if(row.length == 3 && row[0] == "流動負債合計") {
+      amount_current_liability = row[2];
+    } else if(row.length == 3 && row[0] == "固定負債合計") {
+      amount_fixed_liability = row[2];
+    } else if(row.length == 3 && row[0] == category_end) {
+      return true; // ループ終了
+    }
+  });
+  return new Liability(amount_current_liability, amount_fixed_liability);
+}
+
+function netAssetBuilder(text) {
+  const category_begin = "純資産の部";
+  const category_end = "負債純資産合計"
+  let amount_net_asset = 0;
+  const category_data = text.slice(text.indexOf(category_begin) + category_begin.length).trim();
+  const rows = category_data.split('\n');
+  rows.some(function(row_str) {
+    let row = row_str.split(' ').filter(r => r !== "");
+    if(row.length == 3 && row[0] == "純資産合計") {
+      amount_net_asset = row[2];
+    } else if(row.length == 3 && row[0] == category_end) {
+      return true; // ループ終了
+    }
+  });
+  return new NetAsset(amount_net_asset);
+};
+
 if( process.argv.length > 1 ){
   var filename = process.argv[2];
   var buf = fs.readFileSync( filename );
   pdf(buf, options).then( function( data ){
-    var text = data.text;
-    console.log( text );
+    var text = data.text.trim();
+    const asset = assetBuilder(text);
+    const liability = liabilityBuilder(text);
+    const net_asset = netAssetBuilder(text);
+    console.log(`流動資産合計 : ${asset.amount_current_asset}`);
+    console.log(`固定資産合計 : ${asset.amount_fixed_asset}`);
+    console.log(`資産合計 : ${asset.amount_all_asset}`);
+    console.log(`流動負債合計 : ${liability.amount_current_liability}`);
+    console.log(`固定負債合計 : ${liability.amount_fixed_liability}`);
+    console.log(`負債合計 : ${liability.amount_all_liability}`);
+    console.log(`純資産合計 : ${net_asset.amount_net_asset}`);
   }).catch( function( err ){
     console.log( err );
   });
